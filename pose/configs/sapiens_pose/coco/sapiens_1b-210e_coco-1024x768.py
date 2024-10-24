3e829ac27476e4a70b6a01f85e487492afe02df1
Rawal Khirodkar <rawalk@fb.com>
2024-10-24 10:29:14 -0700

    Add copyright header to kp17 configs

--- pose/configs/sapiens_pose/coco/sapiens_1b-210e_coco-1024x768.py
+++ pose/configs/sapiens_pose/coco/sapiens_1b-210e_coco-1024x768.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 _base_ = ['../../_base_/default_runtime.py']
 
 ##-----------------------------------------------------------------
